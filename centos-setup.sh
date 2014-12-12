ebbf62cb671b8ef4d15a0b0a08f421025f2f9f57
rygim <ry.gimmy@gmail.com>
2014-12-12 10:49:57 -0500

    more packages on original centos setup

--- centos-setup.sh
+++ centos-setup.sh
@@ -4,7 +4,11 @@ set -x
 wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
 
-yum install -y git tmux ppp pptp vpnc openvpn NetworkManager-pptp* NetworkManager-vpnc* NetworkManager-openvpn* NetworkManager*
+yum install -y git tmux ppp pptp vpnc openvpn NetworkManager-pptp* NetworkManager-vpnc* NetworkManager-openvpn* NetworkManager* putty dkms kernel-devel httpd git nodejs npm libuuid-devel libtool zip unzip ntpd
+yum groupinstall "Development Tools" -y
+
+chkconfig iptables off
+service iptables stop
 
 cd ~
 mkdir repos
