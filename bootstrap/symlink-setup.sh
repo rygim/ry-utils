00d859995508846d46bc9bff5d90ab3532d38d5c
rygim <ry.gimmy@gmail.com>
2014-12-22 12:27:33 -0500

    change structure of repository

---
+++ bootstrap/symlink-setup.sh
@@ -0,0 +1,13 @@
+TOP_DIR=$(cd $(dirname "$0") && pwd)
+REPO_TOP_DIR=$TOP_DIR/..
+
+for file in  ~/.bashrc ~/.tmux.conf ~/.vimrc ~/.gitconfig
+do
+  [ -f $file ] && rm $file
+  [ -L $file ] && unlink $file
+done
+
+ln -s $REPO_TOP_DIR/dotfiles/bashrc ~/.bashrc
+ln -s $REPO_TOP_DIR/dotfiles/tmux.conf ~/.tmux.conf
+ln -s $REPO_TOP_DIR/dotfiles/vimrc ~/.vimrc
+ln -s $REPO_TOP_DIR/dotfiles/gitconfig ~/.gitconfig
